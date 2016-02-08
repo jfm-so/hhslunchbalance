0ecded6b168133a3693ca639eb65076de3c36b31
Johnathan Martin <mrjohnathanmartin@gmail.com>
2016-02-08 08:54:13 -0600

    Update formatting

--- start.php
+++ start.php
@@ -3,18 +3,8 @@ include('balance.php');
 include('config.php');
 include('gencookie.php');
 if ($dbactive == "true") {
-echo "\nDB enabled\n";
-
-include('db.php');
+    echo "\nDB enabled\n";
+    include('db.php');
 }
 if ($sgactive == "true") {
-echo "\nSendgrid Active\n";
- 
-include('email.php');
-}
-
-if ($twilioactive == "true") {
-echo "\nTwilio Active\n";
-
-include('twilio.php');
-}
+    echo "\nSendgrid Active\n";
